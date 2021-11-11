881d7ab0892080f9322b83f77aea8d6b2fd6e91e
arushpawar1102 <90510338+arushpawar1102@users.noreply.github.com>
2021-11-11 16:28:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -75,29 +75,70 @@ function draw() {
   playerArcher.display();
   computerArcher.display()
 
- // Use for loop to display arrow using showArrow() function
- for (var i = 0; i < playerArrows.length; i++) {
-  showArrows(i, playerArrows);
-}
+ // Uncomment and use correct for loop to display arrow using showArrow() function
+//  for (var i=0; i<playerArrows.length; i++) 
+//  {
+//  showArrows(i, playerArrows);
+//  }
 
-}
+ // for (var i=0; i<playerArrows; i++) 
+ //{
+ // showArrows(i, playerArrows);
+ // }
 
-function keyPressed() {
-
-  if(keyCode === 32){
-    // create an arrow object and add into an array ; set its angle same as angle of playerArcher
-    var posX = playerArcher.body.position.x;
-    var posY = playerArcher.body.position.y;
-    var angle = playerArcher.body.angle+PI/2;
-
-    var arrow = new PlayerArrow(posX, posY, 100, 10);
-
-    arrow.trajectory = [];
-    Matter.Body.setAngle(arrow.body, angle);
-    playerArrows.push(arrow);
-
+  for (var i=0; i<playerArrows.length; i++) 
+ {
+  showArrows(playerArrows, i);
   }
+
+
+
+
+
 }
+
+/*********** Choose correct keyPressed() function out of these *************/
+
+// function keyPressed() {
+//     // create an arrow object and add into an array ; set its angle same as angle of playerArcher
+//     var posX = playerArcher.body.position.x;
+//     var posY = playerArcher.body.position.y;
+//     var angle = playerArcher.body.angle+PI/2;
+//     var arrow = new PlayerArrow(posX, posY, 100, 10);
+//     arrow.trajectory = [];
+//     Matter.Body.setAngle(arrow.body, angle);
+//     playerArrows.push(arrow);
+// }
+
+
+ function keyPressed() {
+   if(keyCode === 32){
+     //create an arrow object and add into an array ; set its angle same as angle of playerArcher
+     var posX = playerArcher.body.position.x;
+     var posY = playerArcher.body.position.y;
+     var angle = playerArcher.body.angle+PI/2;
+     var arrow = new PlayerArrow(posX, posY, 100, 10);
+     arrow.trajectory = [];
+     Matter.Body.setAngle(arrow.body, angle);
+     playerArrows.push(arrow);
+   }
+ }
+
+
+// function keyPressed() {
+//   if(keyCode === 32){
+//     // create an arrow object and add into an array ; set its angle same as angle of playerArcher
+//     var posX = playerArcher.body.position.x;
+//     var posY = playerArcher.body.position.y;
+//     var angle = playerArcher.body.angle+PI/2;
+//     Matter.Body.setAngle(arrow.body, angle);
+//     playerArrows.push(arrow);
+//   }
+// }
+
+
+
+
 
 function keyReleased () {
 
